c91922846fdea1c24139b572c03395c4feb5379d
rifleking <rifleking1@gmail.com>
2025-05-30 13:59:33 +0530

    added backend functionality and UI changes

--- frontend/src/components/stockStats.tsx
+++ frontend/src/components/stockStats.tsx
@@ -1,15 +1,15 @@
 type StockStatProps = {
-    label: string
-    value: string | number
-  }
-  
-  const StockStats: React.FC<StockStatProps>  = ({ label, value }) => { 
-    return (
-      <div className="flex flex-col flex-1 min-w-48">
-        <div className="text-sm text-gray-600">{label}</div>
-        <div className="text-lg font-semibold text-gray-900">{value}</div>
-      </div>
-    )
-  }
+  label: string
+  value: string | number
+}
 
-  export default StockStats;
+const StockStats: React.FC<StockStatProps>  = ({ label, value }) => {
+  return (
+    <div className="flex flex-col justify-between w-36">
+      <div className="text-sm text-gray-600">{label}</div>
+      <div className="text-lg font-semibold text-gray-900">{value}</div>
+    </div>
+  )
+}
+
+export default StockStats;
